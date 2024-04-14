0e7d11ecc5d9377d611ab4afc610b408da422602
Varsha9898 <166931038+Varsha9898@users.noreply.github.com>
2024-04-14 18:41:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,11 +20,14 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', (req, res) => {
+  const ip = req.headers["x-forwarded-for"]
+  const lang = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
 
+  res.json({ipaddress: ip, language: lang, software})
+})
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
